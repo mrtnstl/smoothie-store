7165d4b29f8f3fac48eea31e8e6c83534c451808
mrtnstl <spitellermarton@gmail.com>
2025-12-19 13:27:30 +0100

    add: test

--- src/routes/smoothieRoutes.test.js
+++ src/routes/smoothieRoutes.test.js
@@ -9,7 +9,6 @@ describe("Smoothie Routes", () => {
   const app = express();
   app.use(express.json());
   app.use(smoothieRoutes);
-  //app.listen(3000);
 
   describe("GET /smoothies", () => {
     it("should return the list of smoothies when called", async () => {
@@ -29,7 +28,6 @@ describe("Smoothie Routes", () => {
       const response = await request(app).get("/smoothie/random");
 
       expect(response.status).toBe(200);
-      expect(response.body).toBeDefined();
       expect(response.body.id).toBeDefined();
       expect(response.body.name).toBeDefined();
       expect(response.body.description).toBeDefined();
@@ -40,15 +38,20 @@ describe("Smoothie Routes", () => {
   describe("GET /smoothie/:id", () => {
     it("should return the smoothie of the specified ID", async () => {
       const id = "SM-024";
+      const expected = await Smoothies.findOne("id", id);
       const response = await request(app).get(`/smoothie/${id}`);
 
       expect(response.status).toBe(200);
-      expect(response.body).toBeDefined();
-      expect(response.body.id).toBeDefined();
-      expect(response.body.name).toBeDefined();
-      expect(response.body.description).toBeDefined();
-      expect(response.body.ingredients).toBeDefined();
-      expect(response.body.id).toBe(id);
+      expect(response.body).toEqual(expected);
+    });
+
+    it("should return 404 if the there is no smoothie with the specified ID", async () => {
+      const id = "nonexistent";
+      const expected = { message: `Cannot find smoothie with the id of ${id}` };
+      const response = await request(app).get(`/smoothie/${id}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual(expected);
     });
   });
 });
